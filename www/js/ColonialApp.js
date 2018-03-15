62c218ebf9e63b2a6fe3bc8718c2f4881f8dfa1e
fmir86 <fmir_86@hotmail.com>
2018-03-14 21:43:03 -0600

    Difficulty changes

--- www/js/ColonialApp.js
+++ www/js/ColonialApp.js
@@ -4,6 +4,15 @@ var seaBG, hookGroup, hook, light, cord, fishesLayer, seaLayersPack, seaDarkness
 var seaAnimation;
 var bubblesLayer, bubbleImg;
 var stillAlive = true; // TRUE if no collition with fishes
+var difficulty = 1;
+var realStageWidth = 1536;
+var realStageHeight = 2048;
+
+var fishTopPosition = [200, 750, 1150, 1500];
+
+var selectedFishAsset = 0;
+var selectedDir = 0; // 0 = left / 1 = right
+var selectedY = fishTopPosition[2];
 
 function initGame(cvs, stg){
     canvas = cvs;
@@ -28,10 +37,11 @@ function initGame(cvs, stg){
     deepScreen = new createjs.Bitmap("img/deepScreen.png");
     treasure = new createjs.Bitmap("img/treasure.png");
     seaDarkness = new createjs.Shape();
-    seaDarkness.graphics.beginFill("rgba(0,0,0,0.5)").drawRect(0, 0, canvas.width, canvas.height);
+    seaDarkness.graphics.beginFill("rgba(0,0,0,0.5)").drawRect(0, 0, realStageWidth, realStageHeight);
+	console.log("stage width: " + stage.getBounds());
       TweenMax.set(deepScreen, {y:1800});
       TweenMax.set(seaDarkness, {alpha:0});
-      TweenMax.set(treasure, {y:1700, x:200});
+      TweenMax.set(treasure, {y:1700, x:200, alpha:0});
         seaLayersPack.addChild(seaBG);
         seaLayersPack.addChild(seaDarkness);
         seaLayersPack.addChild(deepScreen);
@@ -74,18 +84,25 @@ function initGame(cvs, stg){
 }
 
 function createSeaAnimation(){
-  seaAnimation = new TimelineLite();
+  seaAnimation = new TimelineLite({onUpdate:setDifficulty});
   seaAnimation.set(seaDarkness, {alpha:0});
   seaAnimation.set(light, {alpha:0});
   seaAnimation.set(deepScreen, {y:1800});
-  seaAnimation.set(treasure, {y:1700});
+  seaAnimation.set(treasure, {y:1700, alpha:1});
   seaAnimation.set(fishesLayer, {y:0});
+  seaAnimation.set(bubblesLayer, {y:0});
   seaAnimation.to(seaDarkness, 7, {alpha:1, ease:Power2.easeOut});
   seaAnimation.to(light, 8, {alpha:1}, "-=5");
   seaAnimation.to(deepScreen, 10, {y:-300, ease:Sine.easeInOut}, "-=10");
   seaAnimation.to(fishesLayer, 10, {y:-2000, ease:Sine.easeOut}, "-=10");
+  seaAnimation.to(bubblesLayer, 10, {y:-2000, ease:Sine.easeOut}, "-=10");
   seaAnimation.to(treasure, 3, {y:1150, ease: Sine.easeOut}, "-=3");
-  seaAnimation.pause();
+  seaAnimation.pause();	
+}
+
+function setDifficulty(elem){
+	difficulty = Math.round((seaAnimation.progress() * 3) + 1);
+	console.log("difficulty: " + difficulty);
 }
 
 function createBubblesAnimation(){
@@ -103,14 +120,14 @@ function createBubblesAnimation(){
     bubblesLayer.addChild(bubblePart);
 
     TweenMax.set(bubblePart, {
-      x: getRandomInt(0, canvas.width),
-      y: canvas.height - warning.getBounds().height,
+      x: getRandomInt(0, realStageWidth),
+      y: realStageHeight - warning.getBounds().height + (-bubblesLayer.y),
       scaleX: randomScale,
       scaleY:randomScale
     });
 
     TweenMax.to(bubblePart, getRandomArbitrary(4,8), {
-      y:-bubbleImg.height,
+      y:-bubbleImg.height + (-bubblesLayer.y),
       ease: Linear.easeNone,
       onComplete: function(){ bubblesLayer.removeChild(bubblePart) }
     });
@@ -127,6 +144,7 @@ function showInitScreen(){
 
 function startGame(e){
   stillAlive = true;
+  difficulty = 1;
   createSeaAnimation();
   createBubblesAnimation();
 
@@ -153,6 +171,7 @@ function onNewAnimationFrame(){
 
 
 function goingDown(){
+	
   if(Drag.deepLevel > 500){
     if(seaAnimation.paused()){
         seaAnimation.play();
@@ -204,19 +223,34 @@ function checkSuccess(){
 }
 
 function throwFishes(){
-  var dir = ["left", "right"];
-  if(stillAlive){
-    createFish(dir[getRandomInt(0,1)]);
-    createFish(dir[getRandomInt(0,1)]);
+	
+	if(stillAlive){
+		if(difficulty == 1){
+			TweenMax.delayedCall(getRandomArbitrary(1, 2), throwFishes);
+			selectedY = getRandomNewIndex(0,2, selectedY);
+		}else if(difficulty == 2){
+			TweenMax.delayedCall(getRandomArbitrary(0.6, 1), throwFishes);
+			selectedY = getRandomNewIndex(0,3, selectedY);
+		}else if(difficulty == 3){
+			TweenMax.delayedCall(getRandomArbitrary(0.5, 0.8), throwFishes);
+			selectedY = getRandomNewIndex(1,3, selectedY);
+		}else if(difficulty == 4){
+			TweenMax.delayedCall(getRandomArbitrary(0.5, 0.7), throwFishes);
+			selectedY = getRandomNewIndex(1,3, selectedY);
+		}
+		
+		selectedFishAsset = getRandomNewIndex(1,4, selectedFishAsset);
+		selectedDir = getRandomNewIndex(0,1, selectedDir)
 
-    TweenMax.delayedCall(getRandomArbitrary(2,4), throwFishes);
-  }
+		createFish( selectedFishAsset, selectedDir, selectedY );
+	}
 }
 
-function createFish(origin){
+function createFish(index, origin, yposition){
   // Create Fish
   var fishImg = new Image(), fishCont, finalX;
-  fishImg.src = "img/fish" + getRandomInt(1,4) + ".png";
+  fishImg.src = "img/fish" + index + ".png";
+	console.log(fishImg.src);
 
   fishImg.onload = function(){
 
@@ -225,23 +259,28 @@ function createFish(origin){
     fishCont.regY = this.height/2;
 
     fishesLayer.addChild(fishCont);
-    fishCont.y = getRandomInt(0, canvas.height - warning.getBounds().height - this.height) + (-fishesLayer.y);
+    fishCont.y = fishTopPosition[yposition] + (-fishesLayer.y);
 
-    if(origin === "left"){
+    if(origin === 0){ // LEFT
       initialX = -this.width/2;
-      finalX = canvas.width + this.width/2;
+      finalX = realStageWidth + this.width/2;
       pointingTo = 1;
-    }else if(origin === "right"){
-      initialX = canvas.width + this.width/2;
+    }else if(origin === 1){ // RIGHT
+      initialX = realStageWidth + this.width/2;
       finalX = -this.width + this.width/2;
       pointingTo = -1;
     }
 
     fishCont.x = initialX;
     fishCont.scaleX = pointingTo;
-    TweenMax.to(fishCont, getRandomArbitrary(4,12), {x:finalX, ease:Linear.easeNone, onComplete:function(){
-      fishesLayer.removeChild(fishCont);
-    }});
+	  
+    TweenMax.to(fishCont, getRandomArbitrary(4,12), {
+		x: finalX,
+		ease: Linear.easeNone,
+		
+		onComplete:function(){
+		  fishesLayer.removeChild(fishCont);
+		}});
   }
 }
 
@@ -268,6 +307,7 @@ function onUserFail(){
 }
 
 function resetGame(){
+	
   if(failScreen.alpha == 1){
     TweenMax.to(failScreen, 0.3, {alpha:0, ease:Linear.easeNone});
   }else if(successScreen.alpha == 1){
@@ -283,4 +323,13 @@ function getRandomInt(min, max) {
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+function getRandomNewIndex(lowest, highest, existingValue){
+	var temp;
+    do{
+		temp = getRandomInt(lowest, highest);
+	}while(temp == selectedFishAsset);
+	
+	return temp;
 }
